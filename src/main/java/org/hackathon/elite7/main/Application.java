abb65a85d0902ed7311df366f77e4bec64433ae2
Aditya Kalia <akalia@xebia.com>
2015-01-09 21:55:54 +0530

    Hello controller

--- src/main/java/org/hackathon/elite7/main/Application.java
+++ src/main/java/org/hackathon/elite7/main/Application.java
@@ -8,11 +8,10 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
-@ComponentScan
+@ComponentScan (value = {"org.hackathon.elite7"})
 public class Application {
-
-    public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
-    }
-
+	public static void main(String[] args) {
+		
+		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+	}
 }
